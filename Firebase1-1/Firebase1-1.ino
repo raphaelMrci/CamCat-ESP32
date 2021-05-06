da3ca0cffdea0ce4d7d97427fa4ff9ed489766d6
MERCIE <raphaelmercie4@gmail.com>
2021-05-06 04:00:34 +0200

    Youpiiiii !!!

--- Firebase1-1/Firebase1-1.ino
+++ Firebase1-1/Firebase1-1.ino
@@ -1,5 +1,5 @@
 const char IID[] = "CI5L8P542X35DS45";
-char * anotherName = "";
+String UID;
 bool userConnected = false;
 
 #include <WiFi.h>
@@ -14,8 +14,80 @@ bool userConnected = false;
 
 #define EEPROM_SIZE 512
 
-AsyncWebServer server(80);
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
 
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+AsyncWebServer server(80);
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
+
+String Photo2Base64() {
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      return "";
+    }
+  
+    String imageFile = "data:image/jpeg;base64,";
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+
+    esp_camera_fb_return(fb);
+    
+    return imageFile;
+}
 String readFile(String path)
 {
   File file = SD_MMC.open(path, FILE_READ);
@@ -39,35 +111,63 @@ void printCommands(){
 
 void checkUser(){
   userConnected = EEPROM.read(0) != 255;
-  int i = 0;
-  
-  while (EEPROM.read(i) != 255){ i++; }
 
-  char chars[i+1];
-
-  i=0;
-
-  while (EEPROM.read(i) < 127){
-    chars[i] = (char) EEPROM.read(i);
-    i++;
-  }
-  anotherName = chars;
-  Serial.print("User ID:");
-  Serial.print(anotherName);
-  Serial.println();
-  Serial.println(userConnected);
+  UID = getEEPROM();
+  Serial.print("User ID: ");
+  Serial.println(UID);
 }
 
 
 void setup()
 {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   Serial.begin(115200);
 
   EEPROM.begin(EEPROM_SIZE);
 
   checkUser();
-  checkUser();
-  checkUser();
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+    if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  //drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QQVGA);  // VGA|CIF|QVGA|HQVGA|QQVGA   ( UXGA? SXGA? XGA? SVGA? )
 
   if (!SD_MMC.begin("", true))
   {
@@ -85,8 +185,6 @@ void setup()
   Serial.println(ssid);
   Serial.println(password);
 
-  checkUser();
-
   //WiFi connection
   Serial.print(F("Connecting to WiFi"));
   WiFi.persistent(false);
@@ -97,8 +195,6 @@ void setup()
     Serial.print(F("."));
     delay(500);
   }
-
-  Serial.println(anotherName);
 
   Serial.print(F("\nCONNECTED!\nhttp://"));
   Serial.println(WiFi.localIP());
@@ -126,30 +222,43 @@ void setup()
             [](AsyncWebServerRequest *request) {},
             NULL,
             [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total){
-              if (EEPROM.read(0) != 255){
+              if (userConnected){
                 request->send(403, "text/plain", "CamCat already assiociated to an account.");
 
                 Serial.print("Try to associate to the CamCat even if it is already associated to an account: ");
-                int i = 0;
-
-                while (EEPROM.read(i) != 255){
-                  checkUser();
-                  //Serial.print(" ");
-                  i++;
-                }
-                Serial.println();
               } else {
-                for (size_t i = 0; i < len+1; i++) {
-                  EEPROM.write(i, data[i]);
-                }
-                Serial.println();
-                EEPROM.commit();
+                Serial.print("Data Length: ");
+                Serial.println(len);
+                save(data);
+                checkUser();
                 request->send(200, "text/plain", "CamCat is assiociated successfully.");
               }
             });
-
+            
   server.begin();
   Serial.println("Server started...");
+}
+
+void save(uint8_t* id) {
+  Serial.println(sizeof(id));
+  Serial.println(sizeof(id)*7);
+  for (int i = 0; i < sizeof(id)*7; ++i)
+  {
+    EEPROM.write(i, id[i]);
+  }
+  EEPROM.commit();
+}
+
+String getEEPROM() {
+  String result;
+  int i = 0;
+  
+  while (EEPROM.read(i) != 255) {
+    result += char(EEPROM.read(i));
+    i++;
+  }
+
+  return result;
 }
 
 void loop() {
@@ -173,8 +282,30 @@ void loop() {
     while (Serial.available()>0){ Serial.read(); }
   }
   
-  if (userConnected){
-    Serial.println(anotherName);
+  if (WiFi.status() == WL_CONNECTED) {
+    if (userConnected){
+      HTTPClient http;
+      checkUser();
+
+      http.begin("https://camcat.herokuapp.com/update");
+      http.addHeader("Content-Type", "application/json");
+      http.addHeader("X-camcat-user", UID);
+      http.addHeader("X-camID", IID);
+
+      int httpResponseCode = http.POST("{\"photo\":\"" + Photo2Base64() + "\"}");
+
+      if (httpResponseCode>0){
+        String response = http.getString();
+
+         Serial.println(httpResponseCode);
+         Serial.println(response);
+      } else {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+      }
+
+      http.end();
+    }
   }
-  delay(1000);
+  delay(3000);
 }
